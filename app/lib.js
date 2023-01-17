bd9878c790467f957001aecd1bacfd2f2f83b40e
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-01-17 01:16:30 -0600

    Refector calcRentalCarCost from an import function to a regular function

--- app/lib.js
+++ app/lib.js
@@ -15,6 +15,19 @@ export const calcRentalCarCost = ({ car, person, days }) => {
   return car.dailyRate * days;
 };
 
-export const isLegal2Drink = (person) => person.age >= 21;
 
+
+/*
+This function takes in an object that contains car, person, and days properties as its parameter and uses object destructuring to extract the values. It uses these values to calculate the total cost of renting the car by multiplying the car's daily rate with the number of days, adding the service fee, and adding the tax rate. If the person's age is under 25, it adds an additional $10 per day to the total cost and returns the final total cost.
+*/
+
+function calcRentalCarCost({ car, person, days }) {
+  let totalCost = car.dailyRate * days;
+  totalCost += car.serviceFee;
+  totalCost += (totalCost * car.taxRate);
+  if (person.age < 25) {
+    totalCost += (days * 10);
+  }
+  return totalCost;
+}
 
