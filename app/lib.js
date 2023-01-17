3dea3431d216b847152719240b9b7a60f712a040
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-01-17 01:03:51 -0600

    Delete screaming-snake and transfer code into lib.js

--- app/lib.js
+++ app/lib.js
@@ -1,5 +1,9 @@
-const TAX_RATE = 0.08;
-const SERVICE_FEE = 24.99;
+// screaming snake case - an example of how you can define the variables in SCREAMING_SNAKE_CASE at the top of the file:
+
+TAX_RATE = 0.08
+SERVICE_FEE = 24.99
+
+// Note that these variables will not be exported and can only be used within the same file they are defined in.
 
 export const addOccupation = (person, occupation) => ({
   ...person,
@@ -12,3 +16,5 @@ export const calcRentalCarCost = ({ car, person, days }) => {
 };
 
 export const isLegal2Drink = (person) => person.age >= 21;
+
+
--- app/screaming-snake.js
+++
@@ -1,6 +0,0 @@
-// screaming snake case - an example of how you can define the variables in SCREAMING_SNAKE_CASE at the top of the file:
-
-TAX_RATE = 0.08
-SERVICE_FEE = 0.1
-
-// Note that these variables will not be exported and can only be used within the same file they are defined in.
